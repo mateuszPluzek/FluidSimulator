76d30475d92ffa57348ed888367273098fc7d38e
mateusz.pluzek <mateusz.pluz@gmail.com>
2026-04-25 15:59:39 +0200

    Setup
    Setup code that lacks basic projection matrix

---
+++ 2DFluidSim/FluidParticle.cs
@@ -0,0 +1,9 @@
+﻿using OpenTK.Mathematics;
+using OpenTK.Platform;
+
+namespace _2DFluidSim;
+
+public class FluidParticle
+{
+    
+}
---
+++ 2DFluidSim/Program.cs
@@ -0,0 +1,110 @@
+﻿using OpenTK.Mathematics;
+using OpenTK.Platform;
+using OpenTK.Graphics.OpenGL;
+
+namespace _2DFluidSim;
+
+class Program
+{
+    public static int screenHeight = 800;
+    public static int screenWidth = 1200;
+    
+    static void Main()
+    {
+        // --- OpenGL Setup ---
+        //Toolkit setup
+        ToolkitOptions tkOptions = new ToolkitOptions();
+        Toolkit.Init(tkOptions);
+        //OpenGL API
+        OpenGLGraphicsApiHints apiHints = new OpenGLGraphicsApiHints();
+        WindowHandle window = Toolkit.Window.Create(apiHints);
+        OpenGLContextHandle context = Toolkit.OpenGL.CreateFromWindow(window);
+        //Binding context to the Window
+        Toolkit.OpenGL.SetCurrentContext(context);
+        OpenTK.Graphics.GLLoader.LoadBindings(Toolkit.OpenGL.GetBindingsContext(context));
+        //event queue
+        void HandleEvents(PalHandle? handle, PlatformEventType type, EventArgs args)
+        {
+            switch (args)
+            {
+                case CloseEventArgs closeEvent:
+                    Toolkit.Window.Destroy(window);
+                    break;
+            }
+               
+        }
+        EventQueue.EventRaised += HandleEvents;
+        
+        //window options
+        Toolkit.Window.SetMode(window, WindowMode.Normal); //Setting window mode to normal
+        Toolkit.Window.SetSize(window, new Vector2i(screenWidth,screenHeight));
+        Toolkit.Window.SetTitle(window, "2D Fluid Sim");
+        GL.Viewport(0, 0, screenWidth,screenHeight); //important!!!
+        
+        // --- Setup Code ---
+        Shader shader = new Shader(); //shader
+        shader.Setup();
+        shader.Use();
+        GL.ClearColor(0.1f, 0.1f, 0.1f, 1.0f); //background
+        GL.Enable(EnableCap.DepthTest); //Enables Depth Test for correct rendering
+    
+        //static point for test
+        Vector3 center = (0.0f, 0.0f, 0.0f);
+        Vector3[] vertices = GenerateCircle(center, 0.01f);
+        
+        // --- Vertex Array Object Setup ---
+        //VAO (references objects)
+        int vao = GL.GenVertexArray();
+        GL.BindVertexArray(vao);
+        //VBO (buffer for VAO that stores the actual data)
+        int vbo = GL.GenBuffer();
+        GL.BindBuffer(BufferTarget.ArrayBuffer, vbo);
+        GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * Vector3.SizeInBytes, vertices, BufferUsage.StaticDraw);
+        //connecting openGL shaders and vbo
+        uint position = (uint)GL.GetAttribLocation(shader.Id, "vPosition"); //getting index of the field from OpenGL
+        GL.EnableVertexAttribArray(position); //telling openGL that data is coming from VAO
+        GL.VertexAttribPointer(position, 3, VertexAttribPointerType.Float, false, sizeof(float) * 3, 0);
+        
+        // --- Camera Setup ---
+
+        //getting field index
+        int viewUniform = GL.GetUniformLocation(shader.Id, "view");
+        int projectionUniform = GL.GetUniformLocation(shader.Id, "projection");
+        int modelUniform = GL.GetUniformLocation(shader.Id, "model");
+        
+        // --- Main Loop ---
+        while (true)
+        {
+            // --- Loop Code ---
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit); //clearing buffer with color
+            
+            GL.DrawArrays(PrimitiveType.TriangleFan, 0, vertices.Length); //drawing
+            
+            Toolkit.OpenGL.SwapBuffers(context); //swap back and front buffers
+            //Event Handling
+            Toolkit.Window.ProcessEvents(false);
+            if (Toolkit.Window.IsWindowDestroyed(window))
+            {
+                break;
+            }
+        }
+    }
+
+    public static Vector3[] GenerateCircle(Vector3 center, float radius, int segments = 32)
+    {
+        Vector3 [] vertices = new Vector3[segments + 2];
+        //Center point
+        vertices[0] = center;
+        //Points around the circle
+        float angleStep = 2 * MathF.PI / segments;
+        for (int i = 0; i <= segments; i++)
+        {
+            float angle = angleStep * i;
+            float x = center.X + radius * MathF.Cos(angle);
+            float y = center.Y + radius * MathF.Sin(angle);
+            vertices[i + 1] = new Vector3(x, y, center.Z);
+        }
+        return vertices;
+    }
+}
+
